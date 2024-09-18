98c96cf859f890dfb9516ffd7f2c73522e55d58a
Florin Nania <florinnania@yahoo.com>
2024-09-18 09:32:07 +0300

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,7 +8,7 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
-app.use('/task', taskRouter);
+app.use('/', taskRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
